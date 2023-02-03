3be63d2f68727013e1986447c0877846cdc7fc2d
Lucas Charoing <charoing.lucas@gmail.com>
2023-02-03 18:21:59 +0100

    Improve performances

--- script.py
+++ script.py
@@ -5,6 +5,8 @@ import ujson
 import random
 from machine import Pin, ADC
 
+lastButtonValue = 1
+
 zoomPin = ADC(Pin(28))
 xAxis = ADC(Pin(27))
 yAxis = ADC(Pin(26))
@@ -16,27 +18,23 @@ controllerId = ''.join(random.choice(lettersAndNumbers) for i in range(10))
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 
-ssid = 'YOUR_SSID'
+ssid = 'YOUR_SSIS'
 password = 'YOUR_PASSWORD'
 wlan.connect(ssid, password)
 url = "http://YOUR_SERVER_IP_ADDRESS:3000/api/control"
 
 
 while True:
-    print(button.value())
     try:
         x = round((xAxis.read_u16() / 65535 - 0.5) * 2, 3)
         y = round((yAxis.read_u16() / 65535 - 0.5) * 2, 3)
         zoom = round(zoomPin.read_u16() / 65535, 1)
-
-        print('POST')
         r = urequests.post(url, headers={'Content-type': 'application/json'}, json={
             'controllerId': controllerId,
             'x': x,
             'y': y,
-            'zoom': zoom  
+            'zoom': zoom
         })
-        print(r.json())
         r.close()
     except Exception as e:
         print(e)
--- server/resources/js/index.js
+++ server/resources/js/index.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:3000");
+const socket = io('http://localhost:3000');
 const canvas = document.querySelector('#render');
 const scene = new THREE.Scene();
 
@@ -17,36 +17,40 @@ resizeCanvas();
 let controllerId;
 
 socket.on('hello', (arg) => {
-    console.log(arg);
-})
+  console.log(arg);
+});
 
 socket.on('control', (arg) => {
-    controllerId ??= arg.controllerId;
-    if (controllerId === arg.controllerId) {
-        targetXRotation += arg.y / 800;
-        targetYRotation += arg.x / 800;
-        targetZoom = arg.zoom * 1.8;
-    }
-})
+  controllerId ??= arg.controllerId;
+  if (controllerId === arg.controllerId) {
+    targetXRotation += arg.y / 800;
+    targetYRotation += arg.x / 800;
+    targetZoom = arg.zoom * 1.8;
+  }
+});
 
 let loader = new THREE.TextureLoader();
-loader.load( './resources/textures/earth_texture.jpg', function ( texture ) {
+loader.load('./resources/textures/earth_texture.jpg', function (texture) {
+  let geometry = new THREE.SphereGeometry(1, 32, 16);
 
-    let geometry = new THREE.SphereGeometry( 1, 32, 16 );
+  let material = new THREE.MeshBasicMaterial({ map: texture, overdraw: 0.5 });
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
 
-    let material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5 } );
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+  tick();
+});
 
-    tick();
-} );
-
-const camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  canvas.width / canvas.height,
+  0.1,
+  100
+);
 camera.position.z = 3;
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas,
 });
 renderer.setSize(canvas.width, canvas.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
@@ -54,21 +58,21 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 const clock = new THREE.Clock();
 
 function tick() {
-    mesh.rotation.x += targetXRotation;
-    mesh.rotation.y += targetYRotation;
-    mesh.position.setZ(mesh.position.z + (targetZoom - mesh.position.z) * 0.01);
-    renderer.render(scene, camera);
-    window.requestAnimationFrame(tick);
+  mesh.rotation.x += targetXRotation;
+  mesh.rotation.y += targetYRotation;
+  mesh.position.setZ(mesh.position.z + (targetZoom - mesh.position.z) * 0.01);
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
 }
 
 window.addEventListener('resize', () => {
-    resizeCanvas();
-    camera.aspect = canvas.width / canvas.height;
-    camera.updateProjectionMatrix();
-    renderer.setSize(canvas.width, canvas.height);
-})
+  resizeCanvas();
+  camera.aspect = canvas.width / canvas.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(canvas.width, canvas.height);
+});
 
 function resizeCanvas() {
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
 }
